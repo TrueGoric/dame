10fe834620e9fb4b32256e3a3642fd31a0238e1d
Marcin Jędasik <marcin.m.jedrasik@gmail.com>
2020-04-05 10:55:24 +0200

    Move register accessors to a separate class.

--- Dame/EmulationState.cs
+++ Dame/EmulationState.cs
@@ -1,54 +1,14 @@
 using System;
-using System.Runtime.InteropServices;
 
 namespace Dame
 {
     class EmulationState : IDisposable
     {
-        private readonly byte[] registers;
-
-        #region Register Accessors
-
-        // nice names for special registers
-        public ref byte Accumulator
-            => ref registers[0];
-        public ref byte Flags
-            => ref registers[1];
-
-        public ref byte A
-            => ref registers[0];
-        public ref byte F
-            => ref registers[1];
-        public ref byte B
-            => ref registers[2];
-        public ref byte C
-            => ref registers[3];
-        public ref byte D
-            => ref registers[4];
-        public ref byte E
-            => ref registers[5];
-        public ref byte H
-            => ref registers[6];
-        public ref byte L
-            => ref registers[7];
-
-        // 16-bit registers
-        public ref ushort BC
-            => ref MemoryMarshal.Cast<byte, ushort>(registers[2..3])[0];
-        public ref ushort DE
-            => ref MemoryMarshal.Cast<byte, ushort>(registers[4..5])[0];
-        public ref ushort HL
-            => ref MemoryMarshal.Cast<byte, ushort>(registers[6..7])[0];
-        public ref ushort SP
-            => ref MemoryMarshal.Cast<byte, ushort>(registers[8..9])[0];
-        public ref ushort PC
-            => ref MemoryMarshal.Cast<byte, ushort>(registers[10..11])[0];
-
-        #endregion
+        public readonly byte[] Registers;
 
         public EmulationState()
         {
-            registers = new byte[12];
+            Registers = new byte[12];
         }
 
         public EmulationState CreateSnapshot()
