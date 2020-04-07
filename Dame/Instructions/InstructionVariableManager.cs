c42d47f4581932f279f99721361cc044b6606771
Marcin Jędrasik <marcin.m.jedrasik@gmail.com>
2020-04-08 00:13:57 +0200

    Add IntructionVariableManager.

---
+++ Dame/Instructions/InstructionVariableManager.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Dame.Opcodes
+{
+    class InstructionVariableManager
+    {
+        private Dictionary<string, ParameterExpression> variables = new Dictionary<string, ParameterExpression>();
+
+        public ParameterExpression Get<T>(string name)
+            where T : unmanaged
+            => variables.ContainsKey(name)
+                ? variables[name]
+                : variables[name] = Expression.Variable(typeof(T), name);
+    }
+}
