988aad9cc4f1ccb8b620ce8369d33bcfbad5b9bf
Marcin Jędrasik <marcin.m.jedrasik@gmail.com>
2020-04-18 22:32:11 +0200

    Add InstructionNotImplementedException.

---
+++ Dame/Exceptions/InstructionNotImplementedException.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Dame.Exceptions
+{
+    class InstructionNotImplementedException : Exception
+    {
+        public int Opcode { get; }
+
+        public InstructionNotImplementedException(int opcode)
+            : this(opcode, "This instruction is not implemented!", null)
+        { }
+
+        public InstructionNotImplementedException(int opcode, string message)
+            : this(opcode, message, null)
+        { }
+
+        public InstructionNotImplementedException(int opcode, string message, Exception innerException)
+            : base($"{message}\n Opcode: 0X{opcode.ToString("X")}", innerException)
+        {
+            Opcode = opcode;
+        }
+    }
+}
