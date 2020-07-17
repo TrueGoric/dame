d6d785b59f0f3cfff69028f024fafbd65a2bc881
Marcin Jędrasik <marcin.m.jedrasik@gmail.com>
2020-07-17 18:30:48 +0200

    List instruction types.

--- Dame.Emulator/Instructions/InstructionType.cs
+++ Dame.Emulator/Instructions/InstructionType.cs
@@ -4,6 +4,24 @@ namespace Dame.Emulator.Instructions
 {
     public enum InstructionType : byte
     {
+        Nop, Stop, Halt,
 
+        Add, AddCarry, Subtract, SubtractCarry,
+        Increment, Decrement,
+        Xor, Or, And, Compare,
+
+        RotateLeft, RotateLeftCarry, RotateRight, RotateRightCarry,
+        ShiftLeft, ShiftRight, ShiftRightLogical,
+        Swap, SetBit, ResetBit, TestBit,
+
+        Load, Push, Pop,
+        
+        Jump, JumpZ, JumpNZ, JumpC, JumpNC,
+        JumpRelative, JumpRelativeZ, JumpRelativeNZ, JumpRelativeC, JumpRelativeNC,
+        Call, CallZ, CallNZ, CallC, CallNC,
+        Return, ReturnZ, ReturnNZ, ReturnC, ReturnNC,
+        EnableInterrupts, DisableInterrupts, ReturnEnableInterrupts,
+
+        DAA, Complement, ComplementCarry, SetCarry
     }
 }
